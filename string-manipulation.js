933cacf686c6de671c77e19e95076632cff4bcdd
annabod <31176989+annabod@users.noreply.github.com>
2017-08-21 10:59:04 +0300

    Create string-manipulation.js

---
+++ string-manipulation.js
@@ -0,0 +1,9 @@
+var String="сАшА аНя";
+var lowerString=String.toLowerCase();
+var firstCharacter=String[0];
+var secoundCharacter=String[5];
+var lowerStringSlice=lowerString.slice(1,4);
+var lowerStringSlice1=lowerString.slice(6);
+var firstCharacterUpper=firstCharacter.toUpperCase();
+var secoundCharacterrUpper=secoundCharacter.toUpperCase();
+firstCharacterUpper+lowerStringSlice+ " "+secoundCharacterrUpper+lowerStringSlice1;
